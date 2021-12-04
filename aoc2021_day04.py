a212fd32a66a4b92cd09679016e01fe260e7da54
aconconi <4670015+aconconi@users.noreply.github.com>
2021-12-04 19:51:36 +0100

    refactoring with generator function

--- aoc2021_day04.py
+++ aoc2021_day04.py
@@ -19,7 +19,7 @@ class Board:
                     return True
         return False
 
-    def is_winning(self):
+    def is_winner(self):
         has_winning_row = any(all(x is None for x in row) for row in self.rows)
         has_winning_col = any(
             all(row[i] is None for row in self.rows) for i in range(self.num_cols)
@@ -30,23 +30,19 @@ class Board:
         return sum(sum(x for x in row if x is not None) for row in self.rows)
 
 
-class Game:
-    def __init__(self, draw_data, boards_data):
-        self.boards = [Board(b) for b in boards_data]
-        self.num_boards = len(self.boards)
-        self.draw = [int(x) for x in draw_data]
-
-    def win_score_gen(self):
-        winners = set()
-        for n in self.draw:
-            if len(winners) == self.num_boards:
-                return
-            for b_idx, board in enumerate(self.boards):
-                if b_idx in winners:
-                    continue
-                if board.cross_out(n) and board.is_winning():
-                    winners.add(b_idx)
-                    yield board.score() * n
+def win_score_gen(draw_data, boards_data):
+    boards = [Board(b) for b in boards_data]
+    num_boards = len(boards)
+    draw = [int(x) for x in reversed(draw_data)]
+    winners = set()
+    while draw and len(winners) < num_boards:
+        n = draw.pop()
+        for b_idx, board in enumerate(boards):
+            if b_idx in winners:
+                continue
+            if board.cross_out(n) and board.is_winner():
+                winners.add(b_idx)
+                yield board.score() * n
 
 
 def parse_input(file_name):
@@ -62,39 +58,36 @@ def parse_input(file_name):
     return draw_data, boards_data
 
 
-def day04_part01(game):
-    return next(game.win_score_gen())
+def day04_part01(score_gen):
+    return next(score_gen)
 
 
-def day04_part02(game):
-    return list(game.win_score_gen())[-1]
-
+def day04_part02(score_gen):
+    return list(score_gen)[-1]
 
 
 @pytest.fixture(autouse=True)
-def fixture_game():
+def fixture_score_gen():
     # Setup tests from puzzle description
     draw_data, boards_data = parse_input("data/day04_test.txt")
-    return Game(draw_data, boards_data)
+    return win_score_gen(draw_data, boards_data)
 
 
-def test_day04_part01(fixture_game):
-    assert day04_part01(fixture_game) == 188 * 24  # 4512
+def test_day04_part01(fixture_score_gen):
+    assert day04_part01(fixture_score_gen) == 188 * 24  # 4512
 
 
-def test_day04_part02(fixture_game):
-    assert day04_part02(fixture_game) == 148 * 13  # 1924
+def test_day04_part02(fixture_score_gen):
+    assert day04_part02(fixture_score_gen) == 148 * 13  # 1924
 
 
 if __name__ == "__main__":
-    bingo = Game(*parse_input("data/day04.txt"))
+    bingo = win_score_gen(*parse_input("data/day04.txt"))
 
     # Part 1
     print("What will your final score be if you choose that board?")
     print(day04_part01(bingo))  # Correct answer is 58412
 
     # Part 2
-    print(
-        "Figure out which board will win last. Once it wins, what would its final score be?"
-    )
+    print("Figure out which board will win last. Once it wins, what would its final score be?")
     print(day04_part02(bingo))  # Correct answer is 10030
