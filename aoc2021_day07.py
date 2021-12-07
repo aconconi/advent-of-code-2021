d9829390276e2168a0cf91a74526503fa7298ca2
aconconi <4670015+aconconi@users.noreply.github.com>
2021-12-07 10:26:13 +0100

    day 7 solution

---
+++ aoc2021_day07.py
@@ -0,0 +1,53 @@
+"""
+    Advent of Code 2021
+    Day 07: The Treachery of Whales
+"""
+
+
+import pytest
+from statistics import median
+
+
+def parse_input(file_name):
+    with open(file_name, "r", encoding="ascii") as data_file:
+        return [int(x) for x in data_file.read().split(",")]
+
+
+def day07_part01(data):
+    med = int(median(data))
+    return sum(abs(x - med) for x in data)
+
+
+def day07_part02(data):
+    def gauss_sum(n):
+        return n * (n + 1) // 2
+
+    def fuel(pos):
+        return sum(gauss_sum(abs(x - pos)) for x in data)
+
+    return min(fuel(pos) for pos in range(len(data)))
+
+
+@pytest.fixture(autouse=True, name="test_data")
+def fixture_test_data():
+    return parse_input("data/day07_test.txt")
+
+
+def test_day07_part01(test_data):
+    assert day07_part01(test_data) == 37
+
+
+def test_day07_part02(test_data):
+    assert day07_part02(test_data) == 168
+
+
+if __name__ == "__main__":
+    input_data = parse_input("data/day07.txt")
+
+    # Part 1
+    print("Day 07 Part 1:")
+    print(day07_part01(input_data))  # Correct answer is 351901
+
+    # Part 2
+    print("Day 07 Part 2:")
+    print(day07_part02(input_data))  # Correct answer is 101079875
